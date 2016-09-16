8d5902ec764ed7918b14f115e56a1df7868f4ed3
stephen.wu <stephen.wu@haizhi.com>
2016-09-16 10:29:26 +0800

    add filter the punctuation

--- word_dictionary.py
+++ word_dictionary.py
@@ -2,8 +2,6 @@
 import codecs
 import os
 import ConfigParser
-from punctuation_mark import Chinese_punctuation
-from punctuation_mark import English_punctuation
 
 
 class WordDictionary(object):
@@ -11,31 +9,37 @@ class WordDictionary(object):
         use to create chinese word dictionary
     """
     def __init__(self):
+        self.punctuation_mark = []
+        self.read_punctuation()
+
         self.key_dictionary = dict()
         self.text_set = list()
-        config_read = ConfigParser()
+        config_read = ConfigParser.ConfigParser()
+        config_read.read("conf/corpus.conf")
         self.path = config_read.get("corpus", "corpus_dir")
+
         self.read_file()
-        for article in self.text_set:
-            for passage in article:
-                self.setup_key_dictionary(passage)
+
+        for passage in self.text_set:
+            self.setup_key_dictionary(passage)
+
+    def read_punctuation(self):
+        file_open = codecs.open("Chinese_punctuation.txt", 'r', 'utf-8')
+        punctuation_set = file_open.readlines()
+        for punctuation in punctuation_set:
+            self.punctuation_mark.append(punctuation[0])
 
     def read_file(self):
         file_set = os.listdir(self.path)
-        # file_set = list()
-        # for i in range(1, 2):
-        #     file_set.append('text' + str(i) + '.txt')
-        for item in file_set:
-            print(item)
-            file_open = codecs.open(item, 'r', 'utf-8')
-            new_text = file_open.readlines()
-            self.text_set.append(new_text)
+        for file_name in file_set:
+            file_open = codecs.open(self.path + '/' + file_name, 'r', 'utf-8')
+            article = file_open.readlines()
+            self.text_set += article
 
     def setup_key_dictionary(self, text):
-        # key_head = text[0]
         is_start = False
-        for i in range(len(text)):
-            if text[i] in Chinese_punctuation or text[i] in English_punctuation:
+        for i in range(len(text) - 1):
+            if text[i] in self.punctuation_mark:
                 is_start = False
             else:
                 if is_start:
@@ -51,9 +55,8 @@ class WordDictionary(object):
                     key_head = key_tail
                 else:
                     key_head = text[i]
+                    is_start = True
 
 
 if __name__ == '__main__':
     my_word_dictionary = WordDictionary()
-    for key in my_word_dictionary.key_dictionary.keys():
-        print(key, my_word_dictionary.key_dictionary[key])
