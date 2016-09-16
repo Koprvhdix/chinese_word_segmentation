e8af09c9eba6644ace4a68f873b52d76e6b50e9b
Koprvhdix <454898060@qq.com>
2016-09-16 09:31:51 +0800

    add conf and new corpus

--- word_dictionary.py
+++ word_dictionary.py
@@ -1,42 +1,57 @@
 # coding:utf-8
 import codecs
+import os
+import ConfigParser
+from punctuation_mark import Chinese_punctuation
+from punctuation_mark import English_punctuation
 
 
 class WordDictionary(object):
+    """
+        use to create chinese word dictionary
+    """
     def __init__(self):
         self.key_dictionary = dict()
         self.text_set = list()
+        config_read = ConfigParser()
+        self.path = config_read.get("corpus", "corpus_dir")
         self.read_file()
-        self.char_ignore = [
-            '，', '。', '：'
-        ]
         for article in self.text_set:
             for passage in article:
                 self.setup_key_dictionary(passage)
 
     def read_file(self):
-        file_set = list()
-        for i in range(1, 2):
-            file_set.append('text' + str(i) + '.txt')
+        file_set = os.listdir(self.path)
+        # file_set = list()
+        # for i in range(1, 2):
+        #     file_set.append('text' + str(i) + '.txt')
         for item in file_set:
             print(item)
-            file_open = codecs.open('data/' + item, 'r', 'utf-8')
+            file_open = codecs.open(item, 'r', 'utf-8')
             new_text = file_open.readlines()
             self.text_set.append(new_text)
 
     def setup_key_dictionary(self, text):
-        key_head = text[0]
-        for i in range(1, len(text)):
-            key_tail = text[i]
-            if key_head not in self.key_dictionary:
-                self.key_dictionary[key_head] = dict()
-                self.key_dictionary[key_head][key_tail] = 1
+        # key_head = text[0]
+        is_start = False
+        for i in range(len(text)):
+            if text[i] in Chinese_punctuation or text[i] in English_punctuation:
+                is_start = False
             else:
-                if key_tail not in self.key_dictionary[key_head]:
-                    self.key_dictionary[key_head][key_tail] = 1
+                if is_start:
+                    key_tail = text[i]
+                    if key_head not in self.key_dictionary:
+                        self.key_dictionary[key_head] = dict()
+                        self.key_dictionary[key_head][key_tail] = 1
+                    else:
+                        if key_tail not in self.key_dictionary[key_head]:
+                            self.key_dictionary[key_head][key_tail] = 1
+                        else:
+                            self.key_dictionary[key_head][key_tail] += 1
+                    key_head = key_tail
                 else:
-                    self.key_dictionary[key_head][key_tail] += 1
-            key_head = key_tail
+                    key_head = text[i]
+
 
 if __name__ == '__main__':
     my_word_dictionary = WordDictionary()
